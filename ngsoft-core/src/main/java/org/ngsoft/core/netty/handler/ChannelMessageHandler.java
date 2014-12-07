3508e0d6988891c3030d350eac1dc7b3615c0976
zwq <zwqlive@163.com>
2014-12-07 18:48:27 +0800

    消息注册器修改

--- ngsoft-core/src/main/java/org/ngsoft/core/netty/handler/ChannelMessageHandler.java
+++ ngsoft-core/src/main/java/org/ngsoft/core/netty/handler/ChannelMessageHandler.java
@@ -3,14 +3,18 @@ package org.ngsoft.core.netty.handler;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 
+import org.ngsoft.core.common.Loggers;
 import org.ngsoft.core.message.IMessage;
 import org.ngsoft.core.service.MessageRegistryService;
 import org.ngsoft.core.session.ISession;
 import org.ngsoft.core.session.NettySession;
+import org.slf4j.Logger;
 
 public class ChannelMessageHandler extends ChannelHandlerAdapter{
 	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static Logger logger = Loggers.MSG_LOGGER;
+	
+	
 	@Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 		if(msg instanceof IMessage){
@@ -21,7 +25,7 @@ public class ChannelMessageHandler extends ChannelHandlerAdapter{
     			handler.session(session).handle(message);
     		}
     	}else{
-    		System.out.println("can not recorgnize message");
+    		logger.error("can not recorgnize message");
     	}
     }
     @Override
--- ngsoft-core/src/main/java/org/ngsoft/core/service/MessageRegistryService.java
+++ ngsoft-core/src/main/java/org/ngsoft/core/service/MessageRegistryService.java
@@ -3,9 +3,11 @@ package org.ngsoft.core.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.ngsoft.core.common.Loggers;
 import org.ngsoft.core.message.IMessage;
 import org.ngsoft.core.message.Message;
 import org.ngsoft.core.netty.handler.IMessageHandler;
+import org.slf4j.Logger;
 
 /**
  * 消息注册服务
@@ -15,9 +17,11 @@ import org.ngsoft.core.netty.handler.IMessageHandler;
  */
 public class MessageRegistryService {
 	
-	public static Map<Integer,Class<? extends Message>> messageClasses = new HashMap<Integer, Class<? extends Message>>();
-	public static Map<Integer,Class<? extends IMessageHandler<? extends IMessage>>> handlerClasses = new HashMap<Integer, Class<? extends IMessageHandler<? extends IMessage>>>();
+	private static Logger logger = Loggers.MSG_LOGGER;
 	
+	private static Map<Integer,Class<? extends Message>> messageClasses = new HashMap<Integer, Class<? extends Message>>();
+	private static Map<Integer,Class<? extends IMessageHandler<? extends IMessage>>> handlerClasses = new HashMap<Integer, Class<? extends IMessageHandler<? extends IMessage>>>();
+	private static Map<Integer,IMessageHandler<? extends IMessage>> handlers = new HashMap<Integer, IMessageHandler<? extends IMessage>>();
 	/**
 	 * 注册消息处理器
 	 * @param messageId
@@ -27,21 +31,44 @@ public class MessageRegistryService {
 	public static void register(int messageId,Class<? extends Message> msgClass, Class<? extends IMessageHandler<? extends IMessage>>  handlerClass){
 		messageClasses.put(messageId, msgClass);
 		handlerClasses.put(messageId, handlerClass);
+		try {
+			handlers.put(messageId, handlerClass.newInstance());
+		} catch (InstantiationException e) {
+			logger.error("initial handler faild! ",e);
+		} catch (IllegalAccessException e) {
+			logger.error("initial handler faild! ",e);
+		}
 	}
 	
-	public static Message getMessage(int msgId) throws InstantiationException, IllegalAccessException{
+	public static Message getMessage(int msgId){
 		if(messageClasses.containsKey(msgId)){
-			return messageClasses.get(msgId).newInstance();
+			try {
+				return messageClasses.get(msgId).newInstance();
+			} catch (InstantiationException e) {
+				logger.error("initial message faild! ",e);
+			} catch (IllegalAccessException e) {
+				logger.error("initial message faild! ",e);
+			}
+			return null;
 		}else{
 			return null;
 		}
 	}
 	
-	public static IMessageHandler<? extends IMessage> getHandler(int msgId) throws InstantiationException, IllegalAccessException{
+	public static IMessageHandler<? extends IMessage> getHandler(int msgId){
+		if(handlers.containsKey(msgId)){
+			return handlers.get(msgId);
+		}
 		if(handlerClasses.containsKey(msgId)){
 			Class<? extends IMessageHandler<? extends IMessage>> handlerClass = handlerClasses.get(msgId);
-			handlerClass.newInstance();
-			return handlerClasses.get(msgId).newInstance();
+			try {
+				handlers.put(msgId, handlerClass.newInstance());
+			} catch (InstantiationException e) {
+				logger.error("initial handler faild! ",e);
+			} catch (IllegalAccessException e) {
+				logger.error("initial handler faild! ",e);
+			}
+			return handlers.get(msgId);
 		}else{
 			return null;
 		}
